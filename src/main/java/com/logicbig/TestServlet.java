158f5ab1c581fde49a1071855e1a7aaf00640f67
clavel <manuelgclavel@gmail.com>
2023-04-26 22:17:09 +0700

    working on it

--- src/main/java/com/logicbig/TestServlet.java
+++ src/main/java/com/logicbig/TestServlet.java
@@ -1,9 +1,23 @@
+package com.logicbig;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @WebServlet(name = "testServlet",
         urlPatterns = {"/test"},
         loadOnStartup = 1)
 public class TestServlet extends HttpServlet {
 
-  @Override
+
+	private static final long serialVersionUID = 1L;
+
+@Override
   protected void doGet (HttpServletRequest req,
                         HttpServletResponse resp)
             throws ServletException, IOException {
